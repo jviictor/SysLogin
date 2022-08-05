bf597c2e23a25aecf227af90fcc9005b523a6b45
jviictor <joaovictorrrr93@gmail.com>
2022-08-05 09:51:52 -0300

    feature/sidebar

---
+++ js/custom_adm.js
@@ -0,0 +1,27 @@
+//Dropdown navbar
+let avatar = document.querySelector('.avatar'),
+    notification = document.querySelector('.notification')
+
+// console.log(avatar);
+
+dropMenu(avatar);
+dropMenu(notification);
+
+function dropMenu(selector){
+  selector.addEventListener("click",()=>{
+    let dropdownMenu = selector.querySelector('.dropdown-menu')
+    dropdownMenu.classList.contains("active")?  dropdownMenu.classList.remove("active") : dropdownMenu.classList.add("active");
+  });
+}
+
+//sidebar toggle
+let sidebar = document.querySelector('.sidebar'),
+    bars = document.querySelector('.bars');
+
+
+bars.addEventListener("click", () => {
+    sidebar.classList.contains("active") ? sidebar.classList.remove("active"):
+    sidebar.classList.add("active");
+})
+
+window.matchMedia("(max-width : 768px)").matches ? sidebar.classList.remove("active"):sidebar.classList.add("active");
